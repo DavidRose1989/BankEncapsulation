d3810c9719c435db06440210220753c6e4d3a8e6
DavidRose <DavidRose331@Gmail.com>
2023-02-14 19:36:03 -0600

    initial

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+
+
+        public double Deposit(double amount)
+        { 
+        
+            return _balance += amount;
+        
+        }
+        public double GetBalance()
+        {
+            
+            return _balance; 
+        
+        }
+
+
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            var bankAccounts = new BankAccount();
+            
+            Console.WriteLine("Deposit amount");
+            var deposit = double.Parse(Console.ReadLine());
+
+
+           bankAccounts.Deposit(deposit);
         }
+
+
     }
 }
